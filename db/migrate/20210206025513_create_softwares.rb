ba6dbb0f33908fb702ec5ef4db59962f32c04b53
makidelmondo <wasichkaufe@hotmail.co.jp>
2021-02-06 13:19:23 +0900

    ソフトウェアDBマイグレーション

--- db/migrate/20210206025513_create_softwares.rb
+++ db/migrate/20210206025513_create_softwares.rb
@@ -1,7 +1,10 @@
 class CreateSoftwares < ActiveRecord::Migration[6.0]
   def change
     create_table :softwares do |t|
-
+      t.string  :title,      null: false
+      t.text    :explanation,null: false
+      t.integer :hardware_id,null: false
+      t.references :user,foreign_key: true
       t.timestamps
     end
   end
