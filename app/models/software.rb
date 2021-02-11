6756ce2933d89ea6da7ec749d4390dd5b2bed168
makidelmondo <wasichkaufe@hotmail.co.jp>
2021-02-11 12:51:47 +0900

    ハードウェアアクティブハッシュアソシエーション

--- app/models/software.rb
+++ app/models/software.rb
@@ -1,2 +1,8 @@
 class Software < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :hardware
+
+  validates :title, :string, presence: true
+  validates :explanation, :text, presence: true
+  validates :hardware_id, numericality: { other_than: 1 }
 end
