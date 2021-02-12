792b3c1004fea6be157cb244b0ce6be4b33f6250
makidelmondo <wasichkaufe@hotmail.co.jp>
2021-02-12 13:39:39 +0900

    ソフトウェア保存機能実装途中、ログイン状態で要検証

--- app/controllers/softwares_controller.rb
+++ app/controllers/softwares_controller.rb
@@ -1,13 +1,28 @@
 class SoftwaresController < ApplicationController
+  before_action :set_software, only: [:edit, :update, :destroy, :show]
   def index
     @softwares = Software.all
   end
 
   def new
-    @softwares = Software.new
+    @software = Software.new
   end
 
   def create
+    @software = Software.new(software_params)
+    if @software.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
+  private
+  def set_software
+    @software = Software.find(params[:id])
+  end
+
+  def software_params
+    params.require(:software).permit(:title, :explanation, :hardware_id, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/hardware.rb
+++ app/models/hardware.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Hardware < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'Switch' },
